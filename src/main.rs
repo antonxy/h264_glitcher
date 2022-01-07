329050b889ace2ef6a8136f936db0f2f6fdcf7ee
Anton Schirg <anton.schirg@posteo.de>
2022-01-07 19:35:35 +0100

    remove table editing in favor of live mode

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ use crate::libh264bitstream::*;
 use std::convert::TryInto;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read, Write};
-use std::io::{Seek, SeekFrom};
 use std::os::raw::c_int;
 use std::path::PathBuf;
 use std::str::FromStr;
@@ -54,265 +53,14 @@ enum_from_primitive! {
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "datamosher", about = "Removes I Frames from h264 stream")]
+#[structopt(name = "h264_glitcher", about = "Live controllable h264 glitcher.",
+            long_about = "Pipe output into 'mpv --untimed --no-cache -'.")]
 struct Opt {
-    #[structopt(short, long, parse(from_os_str))]
-    input: PathBuf,
+    #[structopt(short, long, parse(from_os_str), required=true, help="Input video file(s)")]
+    input: Vec<PathBuf>,
 
-    #[structopt(subcommand)]
-    cmd: Command,
-}
-
-#[derive(Debug, StructOpt)]
-enum Command {
-    CreateEditTable(CreateEditTableCmd),
-    ApplyEditTable(ApplyEditTableCmd),
-    ApplyRandomEdits(ApplyRandomEditsCmd),
-    QuickMode(QuickModeCmd),
-    Streaming(StreamingCmd),
-}
-
-#[derive(Debug, StructOpt)]
-struct CreateEditTableCmd {
-    #[structopt(short, long, parse(from_os_str))]
-    edit_table_out: PathBuf,
-
-    #[structopt(short, long)]
-    frames: Option<usize>,
-}
-
-#[derive(Debug, StructOpt)]
-struct ApplyEditTableCmd {
-    #[structopt(short, long, parse(from_os_str))]
-    edit_table_in: PathBuf,
-    #[structopt(short, long, parse(from_os_str))]
-    output: PathBuf,
-}
-
-#[derive(Debug, StructOpt)]
-struct ApplyRandomEditsCmd {
-    #[structopt(short, long, parse(from_os_str))]
-    edit_table_in: PathBuf,
-
-    #[structopt(short = "o", long, parse(from_os_str))]
-    edit_table_out: PathBuf,
-
-    #[structopt(short = "p", long, default_value = "0.0")]
-    iframe_prob: f32,
-
-    #[structopt(short, long, default_value = "0")]
-    reorder_iframes: usize,
-}
-
-#[derive(Debug, StructOpt)]
-struct QuickModeCmd {
-    #[structopt(short = "p", long, default_value = "0.0")]
-    iframe_prob: f32,
-
-    #[structopt(short, long, default_value = "0")]
-    reorder_iframes: usize,
-
-    #[structopt(short, long, parse(from_os_str))]
-    output: PathBuf,
-}
-
-#[derive(Debug, StructOpt)]
-#[structopt(name = "streaming mode", about = "Live controllable mode", long_about="Pipe output into 'mpv --untimed --no-cache -'. Can be controlled using OSC.")]
-struct StreamingCmd {
-    #[structopt(short = "l", long, default_value = "0.0.0.0:8000")]
+    #[structopt(short = "l", long, default_value = "0.0.0.0:8000", help="OSC listen address")]
     listen_addr: String,
-}
-
-#[derive(Debug)]
-struct EditTableEntry {
-    nal_start: usize,
-    nal_end: usize,
-    iframe: bool,
-}
-
-impl EditTableEntry {
-    fn to_string(&self) -> String {
-        format!(
-            "0x{:06x}-0x{:06x}-{:5}",
-            self.nal_start, self.nal_end, self.iframe
-        )
-    }
-    fn from_string(string: &str) -> EditTableEntry {
-        let mut it = string.split("-");
-        let start_str = it.next().unwrap();
-        let end_str = it.next().unwrap();
-        let iframe_str = it.next().unwrap();
-        let parse_hex =
-            |s: &str| usize::from_str_radix(s.trim().trim_start_matches("0x"), 16).unwrap();
-        let parse_bool = |s: &str| bool::from_str(s.trim()).unwrap();
-        EditTableEntry {
-            nal_start: parse_hex(start_str),
-            nal_end: parse_hex(end_str),
-            iframe: parse_bool(iframe_str),
-        }
-    }
-}
-
-fn create_edit_table(input_file: &mut File, opt: &CreateEditTableCmd) -> std::io::Result<()> {
-    let mut buf = Vec::new();
-    input_file.read_to_end(&mut buf)?;
-
-    let mut edit_table_out = File::create(&opt.edit_table_out)?;
-    unsafe {
-        let h2 = h264_new();
-        let mut start_offset: usize = 0;
-        let mut i = 0;
-        while start_offset < buf.len() && opt.frames.map_or(true, |f| i < f) {
-            let mut nal_start: c_int = 0;
-            let mut nal_end: c_int = 0;
-            find_nal_unit(
-                buf.as_mut_ptr().add(start_offset),
-                (buf.len() - start_offset).try_into().unwrap(),
-                &mut nal_start,
-                &mut nal_end,
-            );
-            let nal_start_glob = nal_start + start_offset as i32;
-            let nal_end_glob = nal_end + start_offset as i32;
-            let nal_size = nal_end - nal_start;
-            let ret = read_nal_unit(
-                h2,
-                buf.as_mut_ptr().add(nal_start_glob.try_into().unwrap()),
-                nal_size,
-            );
-            assert_eq!(ret, nal_size);
-
-            let frame_type = FrameType::from_sh_slice_type((*(*h2).sh).slice_type);
-
-            let entry = EditTableEntry {
-                nal_start: nal_start_glob as usize,
-                nal_end: nal_end_glob as usize,
-                iframe: frame_type == FrameType::IOnly,
-            };
-
-            edit_table_out.write_all(
-                format!(
-                    "{} | dec {:06} | Type {:?}\n",
-                    entry.to_string(),
-                    nal_start_glob,
-                    frame_type
-                )
-                .as_bytes(),
-            )?;
-
-            //let nal_unit_type = NALUnitType::from_i32((*(*h2).nal).nal_unit_type).unwrap();
-            //println!("Frame {:?} - FrameType {:?} - NAL Unit Type {:?} - NAL Size {:}",
-            //         (*(*h2).sh).frame_num,
-            //         frame_type,
-            //         nal_unit_type,
-            //         nal_size
-            //         );
-
-            start_offset += nal_end as usize;
-            i += 1;
-        }
-        h264_free(h2);
-    }
-    Ok(())
-}
-
-fn apply_edit_table(input_file: &mut File, opt: &ApplyEditTableCmd) -> std::io::Result<()> {
-    let mut buf = Vec::new();
-    input_file.read_to_end(&mut buf)?;
-
-    let edit_table_file = File::open(&opt.edit_table_in)?;
-    let edit_table_reader = BufReader::new(edit_table_file);
-
-    let mut outfile = File::create(&opt.output)?;
-    for line in edit_table_reader.lines().map(|l| l.unwrap()) {
-        if line.starts_with("#") {
-            continue;
-        }
-        let entry_str = line.split("|").next().unwrap();
-        let entry = EditTableEntry::from_string(entry_str);
-        outfile.write_all(&buf[(entry.nal_start - 4)..entry.nal_end])?;
-    }
-    Ok(())
-}
-
-struct LineInfo {
-    line: String,
-    entry: EditTableEntry,
-    delete_line: bool,
-}
-
-fn apply_random_edits(opt: &ApplyRandomEditsCmd) -> std::io::Result<()> {
-    let edit_table_file = File::open(&opt.edit_table_in)?;
-    let edit_table_reader = BufReader::new(edit_table_file);
-
-    let mut lines = Vec::new();
-
-    let mut num_iframes = 0;
-    for line in edit_table_reader.lines().map(|l| l.unwrap()) {
-        //if line.starts_with("#") {
-        //    continue;
-        //}
-        let entry_str = line.split("|").next().unwrap();
-        let entry = EditTableEntry::from_string(entry_str);
-        let delete_line =
-            entry.iframe && rand::random::<f32>() > opt.iframe_prob && num_iframes >= 1;
-        if entry.iframe {
-            num_iframes += 1;
-        }
-        lines.push(LineInfo {
-            line: line,
-            entry: entry,
-            delete_line: delete_line,
-        });
-    }
-
-    let num_iframes = lines
-        .iter()
-        .enumerate()
-        .filter(|(_idx, el)| el.entry.iframe && !el.delete_line)
-        .count();
-    for _ in 0..opt.reorder_iframes {
-        let mut start = lines
-            .iter()
-            .enumerate()
-            .cycle()
-            .filter(|(_idx, el)| el.entry.iframe && !el.delete_line);
-        let start_el = start.nth(rand::random::<usize>() % num_iframes).unwrap().0;
-        let end_el = start.nth(4).unwrap().0;
-        println!("Swap {}, {}", start_el, end_el);
-        lines.swap(start_el, end_el);
-    }
-
-    let mut edit_table_out = File::create(&opt.edit_table_out)?;
-    for line in lines {
-        if line.delete_line {
-            edit_table_out.write_all("#".as_bytes())?;
-        }
-        edit_table_out.write_all(format!("{}\n", line.line).as_bytes())?;
-    }
-
-    Ok(())
-}
-
-fn quick_mode(input_file: &mut File, opt: &QuickModeCmd) -> std::io::Result<()> {
-    let create_opts = CreateEditTableCmd {
-        edit_table_out: PathBuf::from("/tmp/edtab"),
-        frames: None,
-    };
-    create_edit_table(input_file, &create_opts)?;
-    let rand_ed = ApplyRandomEditsCmd {
-        edit_table_in: PathBuf::from("/tmp/edtab"),
-        edit_table_out: PathBuf::from("/tmp/edtabo"),
-        iframe_prob: opt.iframe_prob,
-        reorder_iframes: opt.reorder_iframes,
-    };
-    apply_random_edits(&rand_ed)?;
-    let apply_cmd = ApplyEditTableCmd {
-        edit_table_in: PathBuf::from("/tmp/edtabo"),
-        output: opt.output.clone(),
-    };
-    input_file.seek(SeekFrom::Start(0))?;
-    apply_edit_table(input_file, &apply_cmd)?;
-    Ok(())
 }
 
 #[derive(Clone)]
@@ -335,13 +83,22 @@ impl Default for StreamingParams {
 }
 
 
-fn streaming_mode(input_file: &mut File, opt: &StreamingCmd) -> std::io::Result<()> {
+fn main() -> std::io::Result<()> {
+    let opt = Opt::from_args();
+    eprintln!("{:?}", opt);
+
+    // Check if all files can be opened
+    for path in &opt.input {
+        File::open(path)?;
+    }
+
     let streaming_params = Arc::new(Mutex::new(StreamingParams::default()));
+
+    // Run OSC listener
     let addr = match SocketAddrV4::from_str(&opt.listen_addr) {
         Ok(addr) => addr,
         Err(_) => panic!("Invalid listen_addr"),
     };
-
     let streaming_params_cpy = streaming_params.clone();
     thread::spawn(move || {
         osc_listener(&addr, streaming_params_cpy);
@@ -357,6 +114,7 @@ fn streaming_mode(input_file: &mut File, opt: &StreamingCmd) -> std::io::Result<
         Ok(())
     };
 
+    let input_file = File::open(&opt.input[0])?;
     let file = std::io::BufReader::new(input_file);
     let it = NalIterator::new(file.bytes().map(|x| x.unwrap()));
     let mut parser = H264Parser::new();
@@ -446,19 +204,4 @@ fn osc_listener(addr: &SocketAddrV4, streaming_params: Arc<Mutex<StreamingParams
         }
     }
 
-}
-
-fn main() -> std::io::Result<()> {
-    let opt = Opt::from_args();
-    eprintln!("{:?}", opt);
-
-    let mut file = File::open(opt.input)?;
-
-    match opt.cmd {
-        Command::CreateEditTable(opts) => create_edit_table(&mut file, &opts),
-        Command::ApplyEditTable(opts) => apply_edit_table(&mut file, &opts),
-        Command::ApplyRandomEdits(opts) => apply_random_edits(&opts),
-        Command::QuickMode(opts) => quick_mode(&mut file, &opts),
-        Command::Streaming(opts) => streaming_mode(&mut file, &opts),
-    }
 }
